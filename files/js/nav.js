233c2393224bbf0a844485e8970997df2d168d79
Diego Sotorivero <123613958+DSr2111@users.noreply.github.com>
2024-01-15 13:00:43 -0500

    new function for form

--- files/js/nav.js
+++ files/js/nav.js
@@ -23,6 +23,11 @@ function navLoginClick(evt) {
   $signupForm.show();
 }
 
+function navNewStorySubmit(e) {
+  console.debug("navNewStorySubmit", e);
+  $newStoryForm.show();
+}
+
 $navLogin.on("click", navLoginClick);
 
 /** When a user first logins in, update the navbar to reflect that. */
